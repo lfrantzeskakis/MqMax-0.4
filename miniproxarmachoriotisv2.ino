3e58934c0c4538e642b05939e779bca7396a66bd
Lambros Frantzeskakis <lfrantzeskakis@gmail.com>
2017-01-08 18:48:25 +0200

    Add files via upload

---
+++ miniproxarmachoriotisv2.ino
@@ -0,0 +1,86 @@
+
+int enablepin=6;
+int startpin1=5;
+int startpin2=4;
+int ledpin=13;
+int manualpin=8;
+int workingpin=9;
+int standbypin=10;
+int manualmodepin=11;
+int watertable[100];
+int flag=0;
+int long timepass=0;
+int long timesetwork=600000;//10 minutes
+int long timesetwait=40000; //40 sec
+
+boolean istherewater(){
+  int temp=0;
+  float temp1=0;
+  for(int i=0; i<100;i++){
+    delay(100);
+    int sensorValue = analogRead(A0);
+    watertable[i]=sensorValue;
+  }
+  for(int i=0; i<100;i++)
+    temp= watertable[i]+temp;
+   
+  temp1=temp/100;
+float voltage= temp1*(5.0/1023.0);
+if (voltage>2.0)
+return true;
+else
+return false;
+}
+    
+
+
+void setup() {
+ pinMode(enablepin,OUTPUT);
+ pinMode(startpin1,OUTPUT);
+ pinMode(startpin2,OUTPUT);
+ pinMode(ledpin,OUTPUT);
+ Serial.begin(9600);
+ digitalWrite(ledpin,LOW);
+ digitalWrite(enablepin,LOW);
+ digitalWrite(startpin1,LOW);
+ int flag=0;
+ 
+ 
+}
+
+
+void loop() {
+  unsigned long currentMillis = millis();
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+ 
+  
+  if(voltage < 1){
+    digitalWrite(ledpin,LOW);
+    digitalWrite(enablepin,LOW);
+    digitalWrite(startpin1,LOW);
+  }
+  
+  
+  if(voltage > 2.5 && flag == 0){
+    digitalWrite(ledpin,HIGH);
+    digitalWrite(enablepin,HIGH);
+    digitalWrite(startpin1,HIGH);
+    flag=1;
+    }
+    
+   if(manualmodepin ==1 && flag == 0){
+     digitalWrite(ledpin,HIGH);
+    digitalWrite(enablepin,HIGH);
+    digitalWrite(startpin1,HIGH);
+    //time count
+    flag=1;
+   }
+     
+  
+  Serial.print(voltage);
+  Serial.print("| led is : ");
+  Serial.println(digitalRead(ledpin));
+  
+  
+}
---
+++ tameiomusic.ino
@@ -0,0 +1,64 @@
+
+#include "pitches.h"
+const int buttonPin=2;
+const int Relayext=13;
+const int relayr=4;
+
+
+
+int melody[] = {
+  NOTE_C4, NOTE_C5,NOTE_C6, NOTE_C7, NOTE_C8};
+
+
+int noteDurations[] = {
+  4, 5, 6, 7,1};
+  
+void playmusic(){
+  for (int thisNote = 0; thisNote < 5; thisNote++) {
+
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(8, melody[thisNote],noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
+}
+  
+void setup() {
+ playmusic();
+ pinMode(buttonPin, INPUT);
+ pinMode(Relayext, OUTPUT);
+ pinMode(relayr,OUTPUT);
+ digitalWrite(Relayext,LOW);
+ digitalWrite(buttonPin,HIGH);
+ 
+
+ 
+}
+
+void loop() {
+  
+  int buttonState;
+  
+  buttonState=digitalRead(buttonPin);
+  
+  if(buttonState==0){
+    digitalWrite(Relayext,HIGH);
+    digitalWrite(relayr,HIGH);
+    delay(500);
+    digitalWrite(relayr,LOW);
+    playmusic();
+    delay(1000);
+    
+    digitalWrite(Relayext,LOW);
+    digitalWrite(buttonPin,HIGH);
+    //digitalWrite(relayr,LOW);
+    
+  }
+  
+    
+   
+  
+}
+
